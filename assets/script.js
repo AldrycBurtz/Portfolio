0cd30558aae133800c67324dd39a41fbaf290b0d
AldrycBurtz <132442970+AldrycBurtz@users.noreply.github.com>
2023-09-06 12:21:34 +0200

    Added all other files

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+function changeBackgroundImage(imageUrl) {
+    document.body.style.backgroundImage = "url('images/The_Sea_Day.jpg)" + imageUrl + "url(images/image_processing20210321-5690-1pijp0b.jpg')";
+  }
